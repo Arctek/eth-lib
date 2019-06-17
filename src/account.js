01e2174da312a5c402cfe9a4e876fb32899b7fd4
Joshua Richardson <joshua@webcoders.com.au>
2019-06-17 18:47:24 +0930

    Wrapped up secp256k1 curve loading into a deffered function - takes too long in a web browser on page load.

--- src/account.js
+++ src/account.js
@@ -1,11 +1,28 @@
-const Bytes = require("./bytes");
-const Nat = require("./nat");
-const elliptic = require("elliptic");
-const rlp = require("./rlp");
-const secp256k1 = new (elliptic.ec)("secp256k1"); // eslint-disable-line
-const {keccak256, keccak256s} = require("./hash");
+let loaded;
+let Bytes;
+let Nat;
+let elliptic;
+let rlp;
+let secp256k1;
+let keccak256
+let keccak256s;
+let sign27;
+
+function preload() {
+  loaded = true;
+
+  Bytes = require("./bytes");
+  Nat = require("./nat");
+  elliptic = require("elliptic");
+  rlp = require("./rlp");
+  secp256k1 = new (elliptic.ec)("secp256k1"); // eslint-disable-line
+  const hash = require("./hash");
+  keccak256 = hash.keccak256;
+  keccak256s = hash.keccak256s;
+}
 
 const create = entropy => {
+  if (!loaded) preload();
   const innerHex = keccak256(Bytes.concat(Bytes.random(32), entropy || Bytes.random(32)));
   const middleHex = Bytes.concat(Bytes.concat(Bytes.random(32), innerHex), Bytes.random(32));
   const outerHex = keccak256(middleHex);
@@ -13,6 +30,7 @@ const create = entropy => {
 }
 
 const toChecksum = address => {
+  if (!loaded) preload();
   const addressHash = keccak256s(address.slice(2));
   let checksumAddress = "0x";
   for (let i = 0; i < 40; i++)
@@ -23,6 +41,7 @@ const toChecksum = address => {
 }
 
 const fromPrivate = privateKey => {
+  if (!loaded) preload();
   const buffer = new Buffer(privateKey.slice(2), "hex");
   const ecKey = secp256k1.keyFromPrivate(buffer);
   const publicKey = "0x" + ecKey.getPublic(false, 'hex').slice(2);
@@ -34,15 +53,22 @@ const fromPrivate = privateKey => {
   }
 }
 
-const encodeSignature = ([v, r, s]) =>
-  Bytes.flatten([r,s,v]);
+const encodeSignature = ([v, r, s]) => {
+  if (!loaded) preload();
+  return Bytes.flatten([r,s,v]);
+}
 
-const decodeSignature = (hex) => [ 
-  Bytes.slice(64, Bytes.length(hex), hex),
-  Bytes.slice(0, 32, hex),
-  Bytes.slice(32, 64, hex)];
+const decodeSignature = (hex) => {
+  if (!loaded) preload();
+  return [ 
+    Bytes.slice(64, Bytes.length(hex), hex),
+    Bytes.slice(0, 32, hex),
+    Bytes.slice(32, 64, hex)
+  ];
+}
 
 const makeSigner = addToV => (hash, privateKey) => {
+  if (!loaded) preload();
   const signature = secp256k1
     .keyFromPrivate(new Buffer(privateKey.slice(2), "hex"))
     .sign(new Buffer(hash.slice(2), "hex"), {canonical: true});
@@ -55,6 +81,7 @@ const makeSigner = addToV => (hash, privateKey) => {
 const sign = makeSigner(27); // v=27|28 instead of 0|1...
 
 const recover = (hash, signature) => {
+  if (!loaded) preload();
   const vals = decodeSignature(signature);
   const vrs = {v: Bytes.toNumber(vals[0]), r:vals[1].slice(2), s:vals[2].slice(2)};
   const ecPublicKey = secp256k1.recoverPubKey(new Buffer(hash.slice(2), "hex"), vrs, vrs.v < 2 ? vrs.v : 1 - (vrs.v % 2)); // because odd vals mean v=0... sadly that means v=0 means v=1... I hate that
